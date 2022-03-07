9d3bd4a8b0d77eab6cd969a81f86bfdd3fc9a94c
Kyrah Gardner <99275227+kgardner11@users.noreply.github.com>
2022-03-06 22:37:44 -0500

    Added info about the new hyperlink

--- StockAssignment/server.R
+++ StockAssignment/server.R
@@ -5,16 +5,25 @@ shinyServer(function(input, output) {
     
     output$position <- renderPrint({
       if(input$ticker == "WMT"){
+        "Walmart is currently the #1 Fortune 500 Company. Click the link above to view this stock on Nasdaq."} 
       else if(input$ticker == "AMZN"){
+        "Amazon is currently the #2 Fortune 500 Company. Click the link above to view this stock on Nasdaq."} 
       else if(input$ticker == "AAPL"){
+        "Apple is currently the #3 Fortune 500 Company. Click the link above to view this stock on Nasdaq."} 
       else if(input$ticker == "CVS"){
         "CVS Health is currently the #4 Fortune 500 Company."} 
+        "CVS Health is currently the #4 Fortune 500 Company. Click the link above to view this stock on Nasdaq. "} 
       else if(input$ticker == "UNH"){
+        "UnitedHealth Group is currently the #5 Fortune 500 Company. Click the link above to view this stock on Nasdaq."} 
       else if(input$ticker == "MCK"){
+        "McKesson is currently the #7 Fortune 500 Company. Click the link above to view this stock on Nasdaq."} 
       else if(input$ticker == "ABC"){
+        "AmerisourceBergen is currently the #8 Fortune 500 Company. Click the link above to view this stock on Nasdaq."} 
       else if(input$ticker == "GOOGL"){
         "Alphabet is currently the #9 Fortune 500 Company."} 
+        "Alphabet is currently the #9 Fortune 500 Company. Click the link above to view this stock on Nasdaq."} 
       else {
+        "Exxon Mobil is currently the #10 Fortune 500 Company. Click the link above to view this stock on Nasdaq."}
     })
     
     web <- reactive({
