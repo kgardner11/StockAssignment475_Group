9e7b3c87a53295f1791f869023bf7d98c67a64e0
Kyrah Gardner <99275227+kgardner11@users.noreply.github.com>
2022-03-10 08:32:17 -0500

    Established a dashboard layout to make app more aesthetically pleasing

--- StockAssignment/ui.R
+++ StockAssignment/ui.R
@@ -1,42 +1,78 @@
 library(shiny)
 library(shinyWidgets)
+library(shinydashboard)
 library(quantmod)
 library(plotly)
 library(DT)
 library(shinythemes)
 SYMBOLS <- stockSymbols()
 
-ui <- shinyUI(fluidPage(
-  theme= shinytheme("superhero"),
-  titlePanel("Exploring Stocks and the What-If"),
-  img(src = "stockim.png", height = 100, width = 300),
-  selectInput("ticker",h3("Select a Stock Symbol"),
-              choices= names(table(SYMBOLS$Symbol)),
-              selected = 1),
-  fluidRow(column(3, verbatimTextOutput("value"))),
-  
-  dateInput("start","Choose a Start Date",value="1900-01-01",startview="month"),
-  dateInput("end","Choose an End Date",value="2022-03-07",startview="month"),
-  materialSwitch(
-    inputId = "diff",
-    label = "Difference?",
-    status="primary",
-    right = TRUE
+ui <- shinyUI(dashboardPage(
+  skin="red",
+  dashboardHeader(title="Exploring Stocks"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Dashboard",tabName="dashboard",icon=icon("dashboard")),
+      menuItem("Data Table", tabName="dtstock",icon=icon("th")),
+      menuItem("What If?", tabName="whatif",icon=icon("question"))
+    )
   ),
-  htmlOutput("mySite"),
-  verbatimTextOutput("position"),
-  plotOutput("plot"),
-  DT::dataTableOutput("view"),
-  numericInput(
-    "amt",
-    "If I invested this much money (in USD)",
-    10,
-    min=1),
-  dateInput("invest","on this date:",value="2020-03-07",startview="month"),
-  strong(paste("in this stock, and sold it")),
-  dateInput("sell","on this date:", value="2022-03-07",startview="month"),
-  strong(paste("I would have made")),
-  verbatimTextOutput("money")
+  dashboardBody(
+    tabItems(
+      tabItem(tabName="dashboard",
+              fluidRow(
+                box(
+                  title=h3("What Stock Do You Want to Explore?"),width=4,background="black",
+                           selectInput("ticker",h3("Select a Stock Symbol"),
+                                       choices= names(table(SYMBOLS$Symbol)),
+                                       selected = 1),
+                           verbatimTextOutput("value")
+                           ),
+                  box(
+                    title=h3("What Date Range Do You Want to Explore?"),width=4,background="green",
+                    dateInput("start","Choose a Start Date",value="1900-01-01",startview="month"),
+                    dateInput("end","Choose an End Date",value="2022-03-07",startview="month")
+                  ),
+                box(
+                  title=h3("Is This a Top 10 Fortune 500 Company?"),width=4,background="blue",
+                  paste("Click the link to view this stock on Nasdaq."),
+                  htmlOutput("mySite"),
+                  verbatimTextOutput("position")
+                )
+                ),
+              fluidRow(
+                box(
+                  title=h3("Plot of Closing Prices"), width=12,
+                  materialSwitch(
+                    inputId = "diff",
+                    label = "Difference?",
+                    status="primary",
+                    right = TRUE),
+                  plotOutput("plot",height=300)
+                )
+              )
+              ),
+      tabItem(tabName="dtstock",
+              h3("Data Table of Stock Values"),
+              DT::dataTableOutput("view")
+              ),
+      tabItem(tabName="whatif",
+              h3("What if..."),
+              numericInput(
+                "amt",
+                "I invested this much money (in USD)",
+                10,
+                min=1),
+              dateInput("invest","on this date:",value="2020-03-07",startview="month"),
+              strong(paste("in this stock, and sold it")),
+              dateInput("sell","on this date?", value="2022-03-07",startview="month"),
+              strong(paste("Answer: I would have made")),
+              verbatimTextOutput("money")
+              )
+      )
+    )
+  )
 )
-)
-
+  
+  
+  
